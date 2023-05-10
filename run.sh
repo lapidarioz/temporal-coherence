ef8e449b87ea31da168bb0a94a35987ee99bd7b5
lapidarioz <rafael1testa@gmail.com>
2023-05-09 21:18:42 -0300

    Testing tensorflow image

--- run.sh
+++ run.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 # docker build -f Dockerfile.dlib -t temporal .
+# docker build -f Dockerfile.dlib -t tensorflow .
 
 PARENTDIR="$(dirname "$PWD")"
+
 
 nvidia-docker run -it \
     --env LICENSE=yes \
     -p 5555:5555 \
     -v $PWD:/home/jupyter/app \
     -v $PARENTDIR/data:/home/jupyter/data \
-    temporal
+    -w /home/jupyter/app \
+    tensorflow
+
+# nvidia-docker run -it \
+#     --env LICENSE=yes \
+#     -p 5555:5555 \
+#     -v $PWD:/home/jupyter/app \
+#     -v $PARENTDIR/data:/home/jupyter/data \
+#     -w /home/jupyter/app \
+#     temporal
 
 sudo chown -R $USER:$USER $PARENTDIR/data
 sudo chown -R $USER:$USER $PWD
