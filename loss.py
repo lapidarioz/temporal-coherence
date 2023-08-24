e5354b9f125ea074045e97420a8e5f4fb7106f13
lapidarioz <rafael1testa@gmail.com>
2023-08-24 18:57:32 +0000

    Add perceptual loss

--- loss.py
+++ loss.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.models import Model
 from itertools import combinations
 import tensorflow_io as tfio
 from normalize import images_from_normalized
@@ -62,6 +65,11 @@ def coherence_mean_landmarks_loss(previous_y_true_landmarks, y_true_landmarks, p
   loss_pred = loss_function(previous_y_pred_landmarks, y_pred_landmarks)
   return tf.math.reduce_mean(tf.abs(loss_true - loss_pred))
 
+def perceptual_loss(y_true, y_pred):
+    layer_name = 'block5_conv4'
+    intermediate_layer_model = Model(inputs=vgg.input, outputs=vgg.get_layer(layer_name).output)
+    return keras.losses.MSE(intermediate_layer_model(y_true), intermediate_layer_model(y_pred))
+
 class GeneratorLoss(object):
 
     def __init__(self,
@@ -72,7 +80,8 @@ class GeneratorLoss(object):
                 landmarks_loss_function=None,
                 lambda_landmarks_loss=1,
                 landmarks_coherence_loss_function=None,
-                lambda_landmarks_coherence_loss=1):
+                lambda_landmarks_coherence_loss=1,
+                lambda_landmarks_perceptual_loss=None):
         self.main_loss_function = main_loss_function
         self.lambda_main_loss = lambda_main_loss
         self.coherence_loss_function = coherence_loss_function
@@ -82,6 +91,10 @@ class GeneratorLoss(object):
         self.landmarks_coherence_loss_function = landmarks_coherence_loss_function
         self.lambda_landmarks_coherence_loss = lambda_landmarks_coherence_loss
         self.cross_entropy_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        self.lambda_landmarks_perceptual_loss = lambda_landmarks_perceptual_loss
+        if self.lambda_landmarks_perceptual_loss:
+            self.vgg = VGG19(weights='imagenet', include_top=False)
+
 
     def __call__(self,
       disc_generated_output,
@@ -105,6 +118,11 @@ class GeneratorLoss(object):
             coherence_loss = coherence_mean_loss(previous_target, current_target, previous_gen, current_gen, self.coherence_loss_function) * self.lambda_coherence_loss
         else:
             coherence_loss = 0
+          
+        if self.vgg:
+            landmarks_perceptual_loss = perceptual_loss(current_target_landmarks, current_gen_landmarks) * self.lambda_landmarks_perceptual_loss
+        else:
+            landmarks_perceptual_loss = 0
 
         landmarks_loss = 0
         landmarks_coherence_loss = 0
