23df23ec589d6e5fc04dff56792539ed76f2d2c0
Rafael Testa <lapidarioz@users.noreply.github.com>
2024-04-15 18:28:07 -0300

    Add comments

--- normalize.py
+++ normalize.py
@@ -1,11 +1,28 @@
-
 import tensorflow as tf
 
-# Normalizing the images to [-1, 1]
 def normalize_images(sequence):
-  return (sequence / 127.5) - 1
+    """
+    Normalizes a sequence of images (likely an array or tensor of images) to the range [-1, 1].
 
-# Undo normalizing the images to [0, 255]
-@tf.function
+    Args:
+        sequence (tf.Tensor or array-like): The input image data.
+
+    Returns:
+        tf.Tensor: The normalized images within the range [-1, 1]. 
+    """
+
+    return (sequence / 127.5) - 1 
+
+@tf.function  
 def images_from_normalized(sequence):
-  return tf.cast((sequence + 1) * 127.5, tf.uint8)
+    """
+    Converts normalized images (in the range [-1, 1]) back to the standard [0, 255] range.
+
+    Args:
+        sequence (tf.Tensor or array-like): The normalized image data.
+
+    Returns:
+        tf.Tensor: The denormalized images with pixel values in [0, 255], cast as uint8 data type. 
+    """
+
+    return tf.cast((sequence + 1) * 127.5, tf.uint8)  
